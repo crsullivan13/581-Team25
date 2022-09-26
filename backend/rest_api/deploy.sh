b931d67885351a8f72df47ea48d219926e6beade
DerrickQuinnKU <47612550+DerrickQuinnKU@users.noreply.github.com>
2022-09-25 23:07:29 -0500

    Update deploy.sh
    Added documentation

--- backend/rest_api/deploy.sh
+++ backend/rest_api/deploy.sh
@@ -1,3 +1,7 @@
+# deploy.sh by Derrick Quinn
+# Handles everything required to deploy main.py to gcloud app engine. Work in progress, will eventually do more than one thing
+# log: created Sep 25: added gcloud app deploy
+
 #!/bin/bash
 
-gcloud app deploy
+gcloud app deploy # basic "deploy app to gcloud" call that uses the info in app.yaml and cloudbuild.yaml
